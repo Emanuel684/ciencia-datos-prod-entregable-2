c8ed7edecfa37195897377de7046bb270938372a
Emanuel-Beatgig <emanuel@beatgig.com>
2026-04-17 18:04:55 -0500

    fix: correction over test cases

--- mlops_pipeline/src/test.py
+++ mlops_pipeline/src/test.py
@@ -15,5 +15,9 @@ print(df.shape)
 print(df.columns.tolist())
 
 corr = df.corr(numeric_only=True)["Pago_atiempo"].sort_values(ascending=False)
+print("--------------------------------")
+print(corr) 
 
-print(corr)
+print("--------------------------------")
+print("Tests OK")
+print("--------------------------------")
