4df55b6a102f43f1b91012edf4bccaf21a90db42
Emanuel684 <emanuel@beatgig.com>
2026-04-24 13:55:58 -0500

    fix: correction over code implementation

--- mlops_pipeline/src/test.py
+++ mlops_pipeline/src/test.py
@@ -14,7 +14,6 @@ print(df.shape)
 print(df.columns.tolist())
 
 corr = df.corr(numeric_only=True)["Pago_atiempo"].sort_values(ascending=False)
-print("--------------------------------")
 print(corr) 
 
 print("--------------------------------")
